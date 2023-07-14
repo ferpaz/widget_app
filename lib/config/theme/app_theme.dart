eff68ddd32c30e785d98d97b39f688bfd4160b0b
Fernando Paz <ferpaz64@gmail.com>
2023-07-14 10:51:46 -0600

    Adding application theme colors

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.indigo,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.orange,
+  Colors.purple,
+  Colors.deepPurple,
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  })
+  : assert(selectedColor >= 0 && selectedColor < colorList.length, "Color index must be greather than zero and less than ${colorList.length - 1}");
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+  );
+}
